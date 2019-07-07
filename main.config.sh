93a761f74c16aa9a9ef95e172fb95d9d627151d3
Christian Abdelmassih <chrabd@kth.se>
2019-07-07 10:01:57 +0200

    refactor and add gitlab

--- main.config.sh
+++ main.config.sh
@@ -11,3 +11,5 @@ NODES=1
 DOCKER_VERSION="18.06.3~ce~3-0~ubuntu"
 
 USE_NIP_DOMAIN="true"
+
+GITLAB_REGISTRATION_TOKEN=`echo -e "import random,string\ni = string.digits + string.ascii_lowercase\no = ''.join(random.choice(i) for x in range(20))\nprint o" | python`
--- shell-scripts/create_ansible_config.sh
+++ shell-scripts/create_ansible_config.sh
@@ -8,5 +8,5 @@ nodes=$NODES
 EOT
 
   [ ! -z "$USE_NIP_DOMAIN" ] && echo "use_nip_domain=$USE_NIP_DOMAIN" >> ansible_hosts.cfg
-
+  [ ! -z "$GITLAB_REGISTRATION_TOKEN" ] && echo "gitlab_registration_token=$GITLAB_REGISTRATION_TOKEN" >> ansible_hosts.cfg
 }
